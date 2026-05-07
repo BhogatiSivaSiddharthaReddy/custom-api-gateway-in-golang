15206e87b025c7cd7483f44af657b584053e39a4
Siddhartha <bsiddhureddy162001@gmail.com>
2026-05-07 17:37:47 +0530

    DAY 2 Done

--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 )
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request:", r.URL.Path)
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintln(w, `{"message": "Hello World"}`)
+}
+
+func pinghandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Received request:", r.URL.Path)
+	fmt.Fprintln(w, "PONG")
+}
 
 func main() {
 	fmt.Println("Starting API Gateway")
+
+	http.HandleFunc("/", helloWorld)
+	http.HandleFunc("/ping", pinghandler)
+
+	http.ListenAndServe(":8080", nil)
 }
